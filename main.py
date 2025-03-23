00f73fc78e4af2233016be850de8983f592e6d7c
Krushnansh Meher <119305832+Krushnansh22@users.noreply.github.com>
2025-03-23 13:48:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -174,6 +174,6 @@ async def read_root():
     """
     return html_content
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5000)
